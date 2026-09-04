4567da0d10e90e82a8e47bfb04843102aa093f18
adamlaycock <adam.m.laycock@gmail.com>
2026-09-04 17:19:36 +0100

    functionalise the mapping logic from main.py into build_game_map within app_elements.py

--- app_elements.py
+++ app_elements.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import pandas as pd
+from streamlit_folium import st_folium
 from streamlit_gsheets import GSheetsConnection
-from typing import List
+import pandas as pd
+from typing import List, Dict
 from game_functions import get_teams_data, clear_team_data
+from container_management import Container
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 
@@ -57,3 +59,69 @@ def build_team_players(team_names: List[str], team_data: pd.DataFrame):
                 st.write("")
                 for player in team_data[team_data["team_name"]==team_name]["player_name"]:
                     st.write(f"- {player}")
+
+def build_game_map(core_components: Dict[str, Container]) -> None:
+    containers = {
+        "Team A": core_components["team_a_areas"],
+        "Team B": core_components["team_b_areas"],
+        "Team C": core_components["team_c_areas"],
+        "Unclaimed": core_components["unclaimed_areas"],
+    }
+
+    dfs = [
+        pd.DataFrame({
+            "name": [item.name for item in container.items],
+            "geometry": [item.geometry for item in container.items],
+            "is_prot": [item.is_prot for item in container.items],
+            "control": control,
+        })
+        for control, container in containers.items()
+    ]
+
+    full_gdf = gpd.GeoDataFrame(
+        pd.concat(dfs, ignore_index=True),
+        geometry="geometry",
+        crs="EPSG:4326",
+    )
+
+    full_gdf["control"] = pd.Categorical(
+        full_gdf["control"],
+        categories=containers.keys(),
+    )
+    colors = ["#FF0000", "#FFFF00", "#0000FF", "#808080"]
+
+    def style_status(feature):
+        if feature["properties"]["is_prot"]:
+            return {
+                "color": "black",
+                "weight": 4,
+                "fillOpacity": 0.75,
+            }
+
+        return {
+            "color": "#111111",
+            "weight": 1.5,
+            "fillOpacity": 0.25,
+        }
+
+    m = full_gdf.explore(
+        column="control",
+        cmap=colors,
+        categorical=True,
+        tooltip=False,
+        popup=["name", "control", "is_prot"],
+        tiles="OpenStreetMap",
+        style_kwds={"style_function": style_status},
+    )
+
+    bounds = full_gdf.total_bounds
+    minx, miny, maxx, maxy = bounds
+
+    m.fit_bounds([[miny, minx], [maxy, maxx]])
+    m.options["maxBounds"] = [[miny, minx], [maxy, maxx]]
+    m.options["maxBoundsViscosity"] = 1
+    m.options["zoomSnap"] = 0.1
+    m.options["minZoom"] = 12.5
+
+    st_folium(m, width=700, height=500, returned_objects=[])
+    
